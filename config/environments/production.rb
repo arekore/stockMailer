bd19b3715b0ea12ca45fc870176b06265c8dfbcc
arka <arekore0@gmail.com>
2019-11-22 12:11:42 +0900

    本番メール用設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # メール送信用設定
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options   = { host: 'stockmailer-arka.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 end
